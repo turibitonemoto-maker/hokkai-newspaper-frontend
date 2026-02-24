769a6d734ca9b9f913a003ab148a55c7ac00a79e
こーき <turibitonemoto@gmail.com>
2026-02-24 05:23:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7293379319-74783",
+  "appId": "1:820160445583:web:8c5cdc051dec375b9404fa",
+  "apiKey": "AIzaSyCtRptsIIqQ1FASh5ZWU6ExVlCzj1KNJNo",
+  "authDomain": "studio-7293379319-74783.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "820160445583"
+};
