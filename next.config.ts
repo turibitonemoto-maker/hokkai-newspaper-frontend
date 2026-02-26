2f55575e0d417871ff950bcd135b409eb16f5e8b
こーき <turibitonemoto@gmail.com>
2026-02-26 16:37:12 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://assets.

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.st-note.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
