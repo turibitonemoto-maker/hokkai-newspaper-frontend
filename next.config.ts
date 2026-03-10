fd3d24683a11396df5ecc671cc19330fa41cb424
こーき <turibitonemoto@gmail.com>
2026-03-10 16:23:46 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://sconten

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.cdninstagram.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.fbcdn.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
